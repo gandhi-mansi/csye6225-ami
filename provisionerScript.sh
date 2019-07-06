e9181de1c48ab3f5b7e5d1f3d44fb0012f609d1d
shubh1646 <sharma.shubh@northeastern.edu>
2019-07-05 20:11:57 -0400

    little changes in provisional and congig.yml

--- provisionerScript.sh
+++ provisionerScript.sh
@@ -92,8 +92,6 @@ wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 
 
 
-wget https://bucket-name.s3.region-identifier.amazonaws.com/latest/install
-
 
 chmod +x ./install
 
