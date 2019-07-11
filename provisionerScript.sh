b50c180941195f71f32fbc18e251318fec7af20c
shubh1646 <sharma.shubh@northeastern.edu>
2019-07-11 19:05:12 -0400

    final changes

--- provisionerScript.sh
+++ provisionerScript.sh
@@ -95,5 +95,6 @@ wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
 rm -rf install
+
 sudo service codedeploy-agent start
 sudo service codedeploy-agent status
